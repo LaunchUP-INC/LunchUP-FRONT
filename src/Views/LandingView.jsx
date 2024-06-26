8c4faeb584c7de5fb96e67c22019c1e82674c416
LucaNor <lucascythe@gmail.com>
2024-06-26 19:04:40 -0300

    Acomodado el app.jsx

---
+++ src/Views/LandingView.jsx
@@ -0,0 +1,15 @@
+import Landing from "../components/Landing/Landing"
+
+
+const LandingView = () =>{
+
+    return(
+        <div>
+            <Landing />
+        </div>
+    )
+
+
+}
+
+export default LandingView;
