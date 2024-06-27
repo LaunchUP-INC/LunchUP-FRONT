16407fc711127ead4814b1a1cadc523102740d8f
LucaNor <lucascythe@gmail.com>
2024-06-26 23:52:26 -0300

    Nav empezada

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,31 @@
-
+import { useNavigate, useLocation } from "react-router-dom";
+import BagIcon from "../Icons/BagIcon";
+import styles from "./Nav.module.css"
 
 const Nav = () =>{
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    const pathToHideNav = ["/"];
+    const shouldHideNav = pathToHideNav.includes(location.pathname);
+
+
+    
+
+    return shouldHideNav ? null :(
+        <div className={styles.navContainer} >
+            <div className={styles.nav}>
+                <h2 className={styles.logo}>LunchUP <BagIcon className={styles.icon} /></h2>
+                <div className={styles.navLinks}>                   
+                    <ul className={styles.navLinks} >
+                        <li><a onClick={() => navigate("/home")} >Inicio</a></li>
+                        <li><a onClick={() => navigate("/products/add")} >Añadir Producto</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    )
 
 }
 
