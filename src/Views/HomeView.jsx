f6f6cd5ac579e12abeac26bfc0cbe67982a6ca25
Juanma <juanma.baal@gmail.com>
2024-06-28 16:20:00 -0500

    Se agrega el paginado en la HomeView, haciendo uso de 3 cards por pagina, se da algunos estilos al modulo del paginado. Se hacen cambios en el componente Cards para que la informacion de los dishes sea pasada por props y se actualicen las Cards por cada pagina

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,18 +1,45 @@
+
+import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from 'react';
 import Cards from "../components/Cards/Cards";
 import SearchBar from "../components/SearchBar/SearchBar";
-import Filters from "../components/Filters/Filters";
-
+import Pagination from "../components/Pagination/Pagination";
 
 
 const HomeView = () => {
+
+    const dishes = useSelector((state) => state.allProducts);
+
+    // console.log(dishes);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [cardsPerPage] = useState(3);
+
+    //Obtener la cantidad de Cards
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = dishes.slice(indexOfFirstCard, indexOfLastCard);
+
+    console.log('Current Cards: ',currentCards)
+
+    //Cambiar la pagina
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <div>
             <div>
                 <SearchBar />
-                <Filters />
             </div>
             <div>
-                <Cards />
+                <Cards dishes={currentCards}/>
+            </div>
+            <div>
+            <Pagination
+                    currentPage={currentPage}
+                    totalPages={Math.ceil(dishes.length / cardsPerPage)}
+                    onPageChange={paginate}
+                    
+                />
             </div>
         </div>
     )
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,32 @@
+import style from './Pagination.module.css';
+
+const Pagination = ({currentPage, totalPages, onPageChange}) => {
+
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i)
+    }
+
+    return (
+        <div className={style.container}>
+            <nav className={style.paginationContainer}>
+                <ul className={style.pagination}>
+
+                    {pageNumbers.map ((number) => (
+                        <li key={number} className={style.pageItem}>
+                            <button
+                                onClick={() => onPageChange(number)}
+                                className={`${style.pageLink} ${number === currentPage ? style.active : ''}`}
+                            >
+                                {number}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            </div>
+    )
+}
+
+export default Pagination;
