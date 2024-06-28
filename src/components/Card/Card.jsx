1eb56c0ab72df851ba4690267e3d0f946cb3ec49
Juanma <juanma.baal@gmail.com>
2024-06-28 12:38:11 -0500

    Se crea una base de datos local para simular la api, y que nos traiga informacion desde ahi. Se hacen los cambios pertinentes en las actions para actualizar el estado global, y tambien en los componentes Card y Cards para renderizar la informacion

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { getProductDetail } from "../../redux/actions";
 import { useNavigate } from "react-router-dom";
 import styles from "./Card.module.css";
 
-const Card = ({ id, title, image }) => {
+const Card = ({ id, name, image, description }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -16,8 +16,9 @@ const Card = ({ id, title, image }) => {
   return (
     <div onClick={handleOnClick} className={styles.container}>
       <label className={styles.label}>ID: {id}</label>
-      <h3 className={styles.title}>{title}</h3>
-      <img src={image} alt={title}  className={styles.image}/>
+      <h3 className={styles.title}>{name}</h3>
+      <img src={image}   className={styles.image}/>
+      <p className={styles.title}>{description}</p>
     </div>
   );
 };
---
+++ src/utils/db.js
@@ -0,0 +1,209 @@
+ export const dishes = [
+   
+      {
+        "id": 1,
+        "name": "Spaghetti Carbonara",
+        "description": "Clásico plato italiano con huevos, queso, panceta y pimienta negra.",
+        "price": 20,
+        "image": "https://static01.nyt.com/images/2021/02/14/dining/carbonara-horizontal/carbonara-horizontal-square640-v2.jpg",
+        "Meal_Type": {
+          "id": 1,
+          "name": "Almuerzo"
+        },
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "preparationTime": 30,
+        "servings": 4
+      },
+      {
+        "id": 2,
+        "name": "Vegetable Stir Fry",
+        "description": "Salteado de verduras frescas con salsa de soja.",
+        "price": 18,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRglVAdjSKlGW_lqoWhbHtHY7h6jAxjg-kgIw&s",
+        "Meal_Type": {
+          "id": 2,
+          "name": "Cena"
+        },
+        "vegetarian": true,
+        "vegan": true,
+        "glutenFree": true,
+        "dairyFree": true,
+        "preparationTime": 20,
+        "servings": 2
+      },
+      {
+        "id": 3,
+        "name": "Chicken Caesar Salad",
+        "description": "Ensalada clásica con pollo, lechuga romana y aderezo César.",
+        "price": 22,
+        "image": "https://www.jessicagavin.com/wp-content/uploads/2022/06/chicken-caesar-salad-28-1200.jpg",
+        "Meal_Type": {
+          "id": 3,
+          "name": "Ensalada"
+        },
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "preparationTime": 15,
+        "servings": 2
+      },
+      {
+        "id": 4,
+        "name": "Margherita Pizza",
+        "description": "Pizza clásica con tomate, mozzarella y albahaca.",
+        "price": 25,
+        "image": "https://en-chatelaine.mblycdn.com/uploads/ench/2023/07/margherita-pizza.jpg",
+        "Meal_Type": {
+          "id": 1,
+          "name": "Almuerzo"
+        },
+        "vegetarian": true,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "preparationTime": 40,
+        "servings": 4
+      },
+      {
+        "id": 5,
+        "name": "Lentil Soup",
+        "description": "Sopa nutritiva de lentejas con vegetales.",
+        "price": 15,
+        "image": "https://thegreekfoodie.com/wp-content/uploads/2021/01/Greek_lentil_soup_SQ.jpg",
+        "Meal_Type": {
+          "id": 4,
+          "name": "Sopa"
+        },
+        "vegetarian": true,
+        "vegan": true,
+        "glutenFree": true,
+        "dairyFree": true,
+        "preparationTime": 50,
+        "servings": 6
+      },
+      {
+        "id": 6,
+        "name": "Beef Tacos",
+        "description": "Tacos mexicanos con carne de res y vegetales frescos.",
+        "price": 24,
+        "image": "https://danosseasoning.com/wp-content/uploads/2022/03/Beef-Tacos-768x575.jpg",
+        "Meal_Type": {
+          "id": 1,
+          "name": "Almuerzo"
+        },
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": true,
+        "dairyFree": false,
+        "preparationTime": 30,
+        "servings": 4
+      },
+      {
+        "id": 7,
+        "name": "Vegan Burger",
+        "description": "Hamburguesa vegana con garbanzos y vegetales.",
+        "price": 19,
+        "image": "https://minimalistbaker.com/wp-content/uploads/2021/04/Best-Vegan-Burger-SQUARE.jpg",
+        "Meal_Type": {
+          "id": 2,
+          "name": "Cena"
+        },
+        "vegetarian": true,
+        "vegan": true,
+        "glutenFree": false,
+        "dairyFree": true,
+        "preparationTime": 25,
+        "servings": 2
+      },
+      {
+        "id": 8,
+        "name": "Greek Salad",
+        "description": "Ensalada griega con tomates, pepino, cebolla y feta.",
+        "price": 16,
+        "image": "https://www.italianbellavita.com/wp-content/uploads/2022/08/739C7136-CBA2-4DDC-8D56-ECA409F69AB9-3-735x735.jpeg",
+        "Meal_Type": {
+          "id": 3,
+          "name": "Ensalada"
+        },
+        "vegetarian": true,
+        "vegan": false,
+        "glutenFree": true,
+        "dairyFree": false,
+        "preparationTime": 10,
+        "servings": 2
+      },
+      {
+        "id": 9,
+        "name": "Pancakes",
+        "description": "Panqueques esponjosos con miel y frutas.",
+        "price": 12,
+        "image": "https://hips.hearstapps.com/hmg-prod/images/best-homemade-pancakes-index-640775a2dbad8.jpg?crop=0.8890503582601677xw:1xh;center,top&resize=1200:*",
+        "Meal_Type": {
+          "id": 5,
+          "name": "Desayuno"
+        },
+        "vegetarian": true,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "preparationTime": 15,
+        "servings": 4
+      },
+      {
+        "id": 10,
+        "name": "Grilled Salmon",
+        "description": "Salmón a la parrilla con limón y hierbas.",
+        "price": 28,
+        "image": "https://hips.hearstapps.com/hmg-prod/images/how-to-grill-salmon-recipe1-1655870645.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
+        "Meal_Type": {
+          "id": 1,
+          "name": "Almuerzo"
+        },
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": true,
+        "dairyFree": true,
+        "preparationTime": 25,
+        "servings": 2
+      },
+      {
+        "id": 11,
+        "name": "Chocolate Cake",
+        "description": "Pastel de chocolate decadente con glaseado de chocolate.",
+        "price": 30,
+        "image": "https://joyfoodsunshine.com/wp-content/uploads/2020/08/best-chocolate-cake-recipe-from-scratch-8.jpg",
+        "Meal_Type": {
+          "id": 6,
+          "name": "Postre"
+        },
+        "vegetarian": true,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": false,
+        "preparationTime": 60,
+        "servings": 8
+      },
+      {
+        "id": 12,
+        "name": "Avocado Toast",
+        "description": "Tostada de aguacate con semillas y limón.",
+        "price": 10,
+        "image": "https://vancouverwithlove.com/wp-content/uploads/2023/05/high-protein-avocado-toast-featured.jpg",
+        "Meal_Type": {
+          "id": 5,
+          "name": "Desayuno"
+        },
+        "vegetarian": true,
+        "vegan": true,
+        "glutenFree": false,
+        "dairyFree": true,
+        "preparationTime": 10,
+        "servings": 1
+      }
+    ];
+  
+  
