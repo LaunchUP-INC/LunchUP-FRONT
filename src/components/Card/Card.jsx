d425559cd0f9f95da23ca3af5345d708bbf0dba3
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2024-06-30 12:39:42 -0300

    Estilos a las cards y la searchBar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { getProductDetail } from "../../redux/actions";
 import { useNavigate } from "react-router-dom";
 import styles from "./Card.module.css";
 
-const Card = ({ id, name, image, description, price }) => {
+const Card = ({ id, name, image, price }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -15,10 +15,8 @@ const Card = ({ id, name, image, description, price }) => {
 
   return (
     <div onClick={handleOnClick} className={styles.container}>
-      <label className={styles.label}>ID: {id}</label>
       <h3 className={styles.title}>{name}</h3>
-      <img src={image}   className={styles.image}/>
-      <p className={styles.title}>{description}</p>
+      <img src={image} className={styles.image}/>
       <p className={styles.price}>$ {price}</p>
     </div>
   );
