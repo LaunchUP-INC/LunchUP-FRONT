5c470dd039cf9ebbdd284bc9af262e91e15db2d5
LucaNor <lucascythe@gmail.com>
2024-07-02 20:39:24 -0300

    reorganizados los componentes de details

--- src/Views/ProductDetailView.jsx
+++ src/Views/ProductDetailView.jsx
@@ -1,13 +1,32 @@
 import ProductDetail from "../components/ProductDetail/ProductDetail";
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { getProductDetail } from "../redux/actions";
 import styles from "./viewsStyles/ProductDetailView.module.css";
+import Loader from "../components/Loader/Loader";
 
 
 const ProductDetailView = () => {
-    return (
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const productDetail = useSelector((state) => state.productDetail);
+    const [loader, setLoader] = useState(false);
+
+    useEffect(() => {
+        setLoader(true)
+
+        dispatch(getProductDetail(id));
+
+        setLoader(false);
+
+      }, [dispatch, id]);
+
+    return loader || productDetail.id != id ? <Loader /> : (
 
         <div className={styles.container}>
-            <ProductDetail/>
+            <ProductDetail productDetail={productDetail}/>
         </div>
     )
 
