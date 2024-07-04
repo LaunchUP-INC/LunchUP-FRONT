438301d14c65e9be156226cc87f4d58d5981c1a2
GuilleCozzi <109187041+GEdgardoFernandez@users.noreply.github.com>
2024-07-04 15:53:05 -0300

    se renderizo auth0, con errores

--- src/components/SingFormg/SingForm.jsx
+++ src/components/SingFormg/SingForm.jsx
@@ -3,34 +3,37 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { FcGoogle } from "react-icons/fc";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 const SingForm = () => {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Crear Cuenta</h1>
-            <form className={styles.form}>
-                <span>Nombre</span>
-                <input type="text" className={styles.input} placeholder="Ingresa tu nombre"/>
-                <span>Apellido</span>
-                <input type="text" className={styles.input} placeholder="Ingresa tu apellido"/>
-                <span>Email</span>
-                <input type="email" className={styles.input} placeholder="ejemplo@mail.com"/>
-                <span>Contraseña</span>
-                <input type="password" className={styles.input} placeholder="Ingresa tu contraseña"/>
-                <span>Confirmar Contraseña</span>
-                <input type="password" className={styles.input} placeholder="Confirma tu contraseña"/>
-                <button className={styles.btn}>Registrarse</button>
-            </form>
-            <span>Registrase con Google</span>
-            <button className={styles.btnGoogle}>
-                <FcGoogle />
-            </button>
-
-            <p>¿Ya tienes una cuenta? <Link to="/login" className={styles.link}>Iniciar Sesion</Link></p>
-        </div>
+        !isAuthenticated && (
+            <div className={styles.container}>
+                <h1 className={styles.title}>Crear Cuenta</h1>
+                <form className={styles.form}>
+                    <span>Nombre</span>
+                    <input type="text" className={styles.input} placeholder="Ingresa tu nombre" />
+                    <span>Apellido</span>
+                    <input type="text" className={styles.input} placeholder="Ingresa tu apellido" />
+                    <span>Email</span>
+                    <input type="email" className={styles.input} placeholder="ejemplo@mail.com" />
+                    <span>Contraseña</span>
+                    <input type="password" className={styles.input} placeholder="Ingresa tu contraseña" />
+                    <span>Confirmar Contraseña</span>
+                    <input type="password" className={styles.input} placeholder="Confirma tu contraseña" />
+                    <button className={styles.btn}>Registrarse</button>
+                </form>
+                <span>Registrase con Google</span>
+                <button className={styles.btnGoogle} onClick={() => loginWithRedirect()}>
+                    <FcGoogle />
+                </button>
+                
+                <p>¿Ya tienes una cuenta? <Link to="/login" className={styles.link}>Iniciar Sesion</Link></p>
+            </div>
+        )
     )
 }
 
 export default SingForm
-        
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
+
+
+const domain = "dev-qsskpalqd7swrrb7.us.auth0.com";
+const clientId = "yD9h2cJuiY1QtMblCom82CR3YugeqizO";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store = {store}>
+
+    <Provider store={store}>
       <BrowserRouter>
+        <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
         <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+      </Auth0Provider>
+    </BrowserRouter>
+  </Provider>
+  </React.StrictMode >,
 )
