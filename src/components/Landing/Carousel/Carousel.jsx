3ba4320edce719acf836e4ffc85b0a3ab36f47e0
Juanma <juanma.baal@gmail.com>
2024-07-09 18:14:10 -0500

    Se agregan algunos cambio s a la landing Page

--- src/components/Landing/Carousel/Carousel.jsx
+++ src/components/Landing/Carousel/Carousel.jsx
@@ -1,48 +1,42 @@
 import React, { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import { Element } from 'react-scroll';
+import { Container, Row, Col, Carousel, Badge } from 'react-bootstrap';
 import styles from './Carousel.module.css';
 
 const FunctionCarousel  = () => {
-    const [show, setShow] = useState(false);
-
-    const handleShow = () => {
-      setShow(!show);
-    };
   
     return (
-      <Element name="function" className={styles.containerAbout}>
-        <div className={styles.containerSection}>
-          <button onClick={handleShow} className={styles.accordion}>
-            ¿Como funciona?
-          </button>
-          <div className={`${styles.containerSteps} ${show ? styles.open : ''}`}>
-            {show && (
-              <Carousel>
-                <Carousel.Item interval={1000}>
-                  <div className={styles.step}>
-                    <h3>Paso 1: Ingresá tus datos y los de tu hijo.</h3>
-                    <img src="/concept.png" alt="Paso 1" />
-                  </div>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                  <div className={styles.step}>
-                    <img src="/concept3.png" alt="Paso 2" />
-                    <h3>Paso 2: Elegí el menú que más te guste.</h3>
-                  </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <div className={styles.step}>
-                    <h3>Paso 3: Recarga el saldo y paga de manera segura.</h3>
-                    <img src="/concept5.png" alt="Paso 3" />
-                  </div>
-                </Carousel.Item>
-              </Carousel>
-            )}
-          </div>
-        </div>
-      </Element>
-    );
+      <Container id="howItWorks" className={`text-center ${styles.howItWorksContainer}`}>
+          <Row className="justify-content-center">
+              <Col xs={12} className="mb-4">
+                  <h2>
+                   <Badge pill bg="success">¿Cómo funciona?</Badge>
+                  </h2>
+              </Col>
+              <Col xs={12} md={8}>
+                  <Carousel className={styles.carouselCustom}>
+                      <Carousel.Item>
+                          <div className={styles.carouselContent}>
+                              <h4>Paso 1: Ingresá tus datos y los de tu hijo.</h4>
+                              <img src="/concept.png" alt="Paso 1" className={styles.carouselImage} />
+                          </div>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                          <div className={styles.carouselContent}>
+                              <img src="/concept3.png" alt="Paso 2" className={styles.carouselImage} />
+                              <h4>Paso 2: Elegí el menú que más te guste.</h4>
+                          </div>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                          <div className={styles.carouselContent}>
+                              <h4>Paso 3: Recarga el saldo y paga de manera segura.</h4>
+                              <img src="/concept5.png" alt="Paso 3" className={styles.carouselImage} />
+                          </div>
+                      </Carousel.Item>
+                  </Carousel>
+              </Col>
+          </Row>
+      </Container>
+  );
 }
 
 export default FunctionCarousel ;
