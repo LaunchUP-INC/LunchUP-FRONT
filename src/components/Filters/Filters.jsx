8ebc02280a9dccc482c0bf3b8039246d8b83d89d
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2024-07-14 01:00:18 -0300

    Se modificaron los estilos de los filtros

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -3,6 +3,13 @@ import { useDispatch } from "react-redux";
 import styles from "./Filters.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { filterProducts, fetchProducts } from "../../redux/actions";
+import {
+  FaHamburger,
+  FaLeaf,
+  FaCarrot,
+  FaBreadSlice,
+  FaCheese,
+} from "react-icons/fa"; // Ejemplo usando react-icons
 
 const Filters = () => {
   const dispatch = useDispatch();
@@ -14,7 +21,7 @@ const Filters = () => {
   });
 
   const handleFilterChange = (name, value) => {
-    setFilters(prevFilters => ({
+    setFilters((prevFilters) => ({
       ...prevFilters,
       [name]: value,
     }));
@@ -33,33 +40,97 @@ const Filters = () => {
     dispatch(fetchProducts());
   };
 
+  const foodTypes = [
+    { id: "0", name: "Todos", icon: null },
+    {
+      id: "1",
+      name: "Convencional",
+      icon: <FaHamburger className={styles.icon} />,
+    },
+    { id: "2", name: "Vegano", icon: <FaLeaf className={styles.icon} /> },
+    {
+      id: "3",
+      name: "Vegetariano",
+      icon: <FaCarrot className={styles.icon} />,
+    },
+    {
+      id: "4",
+      name: "Sin gluten",
+      icon: <FaBreadSlice className={styles.icon} />,
+    },
+    {
+      id: "5",
+      name: "Sin lacteos",
+      icon: <FaCheese className={styles.icon} />,
+    },
+  ];
+
   return (
     <>
-      <SearchBar 
+      <SearchBar
         searchText={filters.name}
-        onInputChange={(value) => handleFilterChange('name', value)}
+        onInputChange={(value) => handleFilterChange("name", value)}
         onSearch={applyFilters}
         onReset={resetAllFilters}
       />
       <div className={styles.container}>
         <div className={styles.filter}>
-          <span className={styles.label}>Tipo de comida</span>
-          <select name="type" className={styles.select} onChange={(e) => handleFilterChange(e.target.name, e.target.value)}>
-            <option value="0">Todos</option>
-            <option value="1">Convencional</option>
-            <option value="2">Vegano</option>
-            <option value="3">Vegetariano</option>
-            <option value="4">Sin gluten</option>
-            <option value="5">Sin lacteos</option>
+          <select
+            name="type"
+            className={styles.select}
+            value={filters.type}
+            onChange={(e) => handleFilterChange(e.target.name, e.target.value)}
+          >
+            {foodTypes.map((type) => (
+              <option key={type.id} value={type.id}>
+                {type.name}
+              </option>
+            ))}
           </select>
+          <div className={styles.filterOptions}>
+            {foodTypes.map((type) => (
+              <div
+                key={type.id}
+                className={`${styles.filterCard} ${
+                  filters.type === type.id ? styles.selected : ""
+                }`}
+                onClick={() => handleFilterChange("type", type.id)}
+              >
+                {type.icon}
+                <span>{type.name}</span>
+              </div>
+            ))}
+          </div>
         </div>
 
         <div className={styles.filter}>
-          <span className={styles.label}>Ordenar por precio</span>
-          <select name="price" className={styles.select} onChange={(e) => handleFilterChange(e.target.name, e.target.value)}>
+          <select
+            name="price"
+            className={styles.select}
+            value={filters.price}
+            onChange={(e) => handleFilterChange(e.target.name, e.target.value)}
+          >
             <option value="price-desc">Mayor precio</option>
             <option value="price-asc">Menor precio</option>
           </select>
+          <div className={styles.filterOptions}>
+            <div
+              className={`${styles.filterCard} ${
+                filters.price === "price-desc" ? styles.selected : ""
+              }`}
+              onClick={() => handleFilterChange("price", "price-desc")}
+            >
+              <span>Mayor precio</span>
+            </div>
+            <div
+              className={`${styles.filterCard} ${
+                filters.price === "price-asc" ? styles.selected : ""
+              }`}
+              onClick={() => handleFilterChange("price", "price-asc")}
+            >
+              <span>Menor precio</span>
+            </div>
+          </div>
         </div>
 
         <button className={styles.apply} onClick={applyFilters}>
