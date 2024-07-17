489b16babbc27f0f90f716e17c8f5bb5c548f97b
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2024-07-17 15:46:33 -0300

    estilos generales

--- src/components/AdminComponents/AdminNav/SideBar.jsx
+++ src/components/AdminComponents/AdminNav/SideBar.jsx
@@ -1,36 +1,45 @@
-import { Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Sidebar = ({ isOpen, toggle }) => {
-    return (
-        <Nav className={`bg-light sidebar ${isOpen ? 'open' : ''}`} activeKey="/admin">
-            <div className="sidebar-sticky"></div>
-            <div>
-
-            
-            <Nav.Item>
-                <LinkContainer to="/admin">
-                    <Nav.Link onClick={toggle}>Dashboard</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/admin/products">
-                    <Nav.Link onClick={toggle}>Productos</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/admin/users">
-                    <Nav.Link onClick={toggle}>Usuarios</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/admin/stats">
-                    <Nav.Link onClick={toggle}>Estadisticas</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            </div>
-        </Nav>
-    );
+  return (
+    <Nav
+      className={`bg-secondary sidebar ${isOpen ? "open" : ""}`}
+      activeKey="/admin"
+    >
+      <div className="sidebar-sticky"></div>
+      <div>
+        <Nav.Item>
+          <LinkContainer to="/admin">
+            <Nav.Link className=" text-white" onClick={toggle}>
+              Dashboard
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+        <Nav.Item>
+          <LinkContainer to="/admin/products">
+            <Nav.Link className=" text-white" onClick={toggle}>
+              Productos
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+        <Nav.Item>
+          <LinkContainer to="/admin/users">
+            <Nav.Link className=" text-white" onClick={toggle}>
+              Usuarios
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+        <Nav.Item>
+          <LinkContainer to="/admin/stats">
+            <Nav.Link className=" text-white" onClick={toggle}>
+              Estadisticas
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+      </div>
+    </Nav>
+  );
 };
 
 export default Sidebar;
