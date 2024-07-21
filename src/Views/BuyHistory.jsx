dbe006e65560aec48377fe05709b9f00cf2c7c9b
GuilleCozzi <109187041+GEdgardoFernandez@users.noreply.github.com>
2024-07-21 14:19:09 -0300

    se creo el componente para el historial de compras

---
+++ src/Views/BuyHistory.jsx
@@ -0,0 +1,11 @@
+import BuyHistorial from "../components/BuyHistorial/BuyHIstorial";
+
+
+const BuyHistory = () => {
+    return (
+        <div>
+            <BuyHistorial />
+        </div>
+    )
+}
+export default BuyHistory
