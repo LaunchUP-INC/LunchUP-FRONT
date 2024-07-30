ca2a77f80e778f1efeb7f461a6d11e5ff0a7313b
LucaNor <144852201+LucaNorDem@users.noreply.github.com>
2024-07-29 23:27:20 -0300

    Update ProductForm.jsx

--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -136,6 +136,7 @@ const ProductForm = () => {
             Meal_Types: []
           });
           navigate("/admin/products");
+        }
       } catch (error) {
         setLoading(false);
         Swal.fire({
