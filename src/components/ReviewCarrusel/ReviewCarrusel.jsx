0a60e99e4484e8249c18cf65b9c9bfd43404ca72
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2024-07-30 16:38:50 -0300

    feature/reviews

--- src/components/ReviewCarrusel/ReviewCarrusel.jsx
+++ src/components/ReviewCarrusel/ReviewCarrusel.jsx
@@ -1,15 +1,7 @@
-import styles from "./ReviewCarrusel.module.css";
-import { Carousel } from "react-bootstrap";
 import Reviews from "../Reviews/Reviews";
 
 const ReviewCarrusel = () => {
-  return (
-    <Carousel interval={3000} variant="dark" slide={false}>
-      <Carousel.Item>
-        <Reviews />
-      </Carousel.Item>
-    </Carousel>
-  );
+  return <Reviews />;
 };
 
 export default ReviewCarrusel;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,9 +1,9 @@
-import styles from "./Reviews.module.css";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
 import { fetchReviews, fetchUsers } from "../../redux/actions";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Reviews.module.css";
 
 const Reviews = () => {
   const dispatch = useDispatch();
@@ -33,21 +33,50 @@ const Reviews = () => {
     return usersReview;
   };
 
+  const groupReviews = (reviews, groupSize) => {
+    const groupedReviews = [];
+    for (let i = 0; i < reviews.length; i += groupSize) {
+      groupedReviews.push(reviews.slice(i, i + groupSize));
+    }
+    return groupedReviews;
+  };
+
   const usersReviews = compare();
+  const groupedReviews = groupReviews(usersReviews, 3);
+
+  const generateBackgroundStyle = (index) => {
+    const colors = ["#f5f5f0", "#f0e68c", "#e6e6fa", "#f0fff0", "#ffe4e1"];
+    const baseColor = colors[index % colors.length];
+    return {
+      backgroundImage: `linear-gradient(${baseColor} 1.1rem, #ccc 1.2rem)`,
+    };
+  };
 
   return (
     <div>
-      {usersReviews.length > 0 ? (
-        <Carousel interval={5000} controls={false} indicators={false}>
-          {usersReviews.map((userReview) => (
-            <Carousel.Item key={userReview.id}>
-              <div className={styles.page}>
-                <div className={styles.margin}></div>
-                <h2>
-                  {userReview.Name} {userReview.LastName}
-                </h2>
-                <p>{userReview.comment}</p>
-                <p>Score: {userReview.score}</p>
+      {groupedReviews.length > 0 ? (
+        <Carousel
+          interval={5000}
+          controls={false}
+          indicators={true}
+          variant="dark"
+        >
+          {groupedReviews.map((group, index) => (
+            <Carousel.Item key={index}>
+              <div className={styles.group}>
+                {group.map((userReview, reviewIndex) => (
+                  <div
+                    key={userReview.id}
+                    className={styles.page}
+                    style={generateBackgroundStyle(index * 3 + reviewIndex)}
+                  >
+                    <div className={styles.margin}></div>
+                    <h2 className={styles.title}>{userReview.Name}</h2>
+                    <h2> {userReview.LastName}</h2>
+                    <p>{userReview.comment}</p>
+                    <p>Score: {userReview.score}</p>
+                  </div>
+                ))}
               </div>
             </Carousel.Item>
           ))}
